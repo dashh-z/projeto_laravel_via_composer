03c677635124a23fa6ce1fd384fddf849540052c
Luiz <luizphilipesantos4@gmail.com>
2025-06-05 12:00:36 -0300

    .

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -9,24 +9,13 @@ use App\Models\SiteContato;
 class ContatoController extends Controller
 {
     public function contato(Request $request) {
-        /*  
-        $contato = new SiteContato();
-        $contato->nome = $request->input('nome');
-        $contato->telefone = $request->input('telefone');
-        $contato->email = $request->input('email');
-        $contato->motivo_contato = $request->input('motivo_contato');
-        $contato->mensagem = $request->input('mensagem');
 
-        $contato->save();
-        */
-        
-        //$contato = new SiteContato();
-        //$contato->create($request->all());
-        //$contato->save();
-
-        //print_r($contato->getAttributes());
-    
-        return view('site.contato', ['titulo' => 'Contato (teste)']);
+       $motivo_contatos = [
+           '1' => 'Dúvida',
+           '2' => 'Elogio',
+           '3' => 'Reclamação'
+       ];
+        return view('site.contato', ['titulo' => 'Contato (teste)', 'motivo_contatos' => $motivo_contatos]);
 
     }
 
