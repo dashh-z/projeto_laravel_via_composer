01531bb2245e65f40e1e7fd27b66d67096a6a310
Luiz <luizphilipesantos4@gmail.com>
2025-06-09 12:00:01 -0300

    Att 4

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -5,16 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\SiteContato as ModelsSiteContato;
 use Illuminate\Http\Request;
 use App\Models\SiteContato;
+use App\Mo
 
 class ContatoController extends Controller
 {
     public function contato(Request $request) {
 
-       $motivo_contatos = [
-           '1' => 'Dúvida',
-           '2' => 'Elogio',
-           '3' => 'Reclamação'
-       ];
+      $motivo_contatos = MotivoContato::all();
+
         return view('site.contato', ['titulo' => 'Contato (teste)', 'motivo_contatos' => $motivo_contatos]);
 
     }
